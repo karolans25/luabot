c03d834279b350836ddafab9111a256608000ac3
carolinaPG <crpulidog@unal.edu.co>
2016-01-13 00:12:47 -0500

    servidor para 64 bits
     Changes to be committed:
    	modified:   serverNode/init.lua
    		//Archivo de prueba
    	new directory:   serverNode/node-v5.4.1-linux-x64/*
    		//Para realizar la conexión con OS de 64-bit
    
    	/*
    	*  PROBLEMAS PARA RESOLVER:
    	*/
    	En el luaupload.py tengo problemas para cargar el init.lua al ESP8266.
    
    	Este es el error que sale:
    	----------------------------------------------------------------------
    	Server On
    	Se ha escrito correctamente en init.lua
    	stdout:
    	stderr Traceback (most recent call last):
    	  File "./serverNode/luaupload.py", line 23, in <module>
    	    import click
    	ImportError: No module named click
    
    	exec error: Error: Command failed: /bin/sh -c ./serverNode/luaupload.py upload -p /dev/ttyUSB0 -b 9600 ./serverNode/init.lua
    	Traceback (most recent call last):
    	  File "./serverNode/luaupload.py", line 23, in <module>
    	    import click
    	ImportError: No module named click
    	----------------------------------------------------------------------

--- serverNode/init.lua
+++ serverNode/init.lua
@@ -1,39 +1,11 @@
-function intercambio(pines)
-  if (gpio.read(pines)) == 1 then
-    gpio.write(pines, gpio.LOW)
-  else
-    gpio.write(pines, gpio.HIGH)
-  end
+led = 1
+gpio.mode(led, gpio.OUTPUT);
+print('El led es una salida')
+for count = 1, 10 do
+  print('vamos a encender el led')
+  gpio.write(led, gpio.HIGH)
+  tmr.delay(10000000)
+  print('vamos a apagar el led')
+  gpio.write(led, gpio.LOW)
 end
-
-
-bombillo = 0
-LED = 1
-gpio.mode(LED, gpio.OUTPUT);
-wifi.setmode(wifi.SOFTAP);
-wifi.ap.config({ssid="miRed", pwd="12345678"});
-srv=net.createServer(net.TCP)srv:listen(80,function(conn)
-conn:on("receive", function(client,request)
-local buf = "";
-local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
-if(method == nil)then
-_, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
-end
-local _GET = {}
-if (vars ~= nil)then
-for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
-_GET[k] = v
-end
-end
-buf = buf.."<h1>Mi pagina Web</h1>";
-  buf = buf.."<p>Encender LED</p>";
-  buf = buf.."<p><a href=\"?pin=b4\"><button>pulsador</button></a></p>";
-local _on,_off = "",""
-  if (_GET.pin == "b4") then
-    intercambio(LED)
-  end
-client:send(buf);
-client:close();
-collectgarbage();
-end)
-end)
+print('Hemos terminado')
